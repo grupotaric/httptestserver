52bfd80e11c51fec651d14bed7f70440e3e85cf3
Javier Santacruz <jsl@taric.es>
2014-11-12 11:11:01 +0100

    Adds packaging and requirements info

---
+++ setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.dirname(os.path.abspath(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+
+setup(
+    name='httptestserver',
+    version='0.1.0',
+    author='Javier Santacruz',
+    author_email='javier.santacruz.lc@gmail.com',
+    long_description=README,
+    description='HTTP and HTTPS testing server',
+    include_package_data=True,
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Topic :: Software Development :: Testing"
+    ],
+)
