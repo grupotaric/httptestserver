ce2e6682562efa063d83430527d181990bbe69c7
Javier Santacruz <javier.santacruz.lc@gmail.com>
2015-02-14 01:00:46 +0100

    Bumps version 0.2.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = open(os.path.join(here, 'README.rst')).read()
 
 setup(
     name='httptestserver',
-    version='0.2.0',
+    version='0.2.1',
     author='Javier Santacruz',
     author_email='javier.santacruz.lc@gmail.com',
     long_description=README,
